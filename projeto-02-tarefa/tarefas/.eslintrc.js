cb5d2238157444fb3bc3754338f0771a5a09ea75
Jeferson Matheus <jeferson.matheus@live.com>
2021-03-01 23:41:35 -0300

    core: add new rule  to eslint

--- projeto-02-tarefa/tarefas/.eslintrc.js
+++ projeto-02-tarefa/tarefas/.eslintrc.js
@@ -1,26 +1,32 @@
 module.exports = {
-  root: true,
   env: {
-    node: true,
+    browser: true,
+    es2021: true
   },
-  extends: ['plugin:vue/essential', '@vue/standard', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:vue/essential'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'import/extensions': ['error', 'ignorePackages', { vue: 'always' }]
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+      ts: 'never',
+      tsx: 'never',
+      vue: 'never'
+    }]
   },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  plugins: ["import"],
+  plugins: ['import','vue'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       alias: {
-        map: [
-          ["@", "./src"], 
-        ],
-        extensions: [".vue", ".json", ".js"]
+        map: [['@', './src']],
+        extensions: ['*', '.js', '.vue', '.json']
       }
-    }
+    },
   }
 }
